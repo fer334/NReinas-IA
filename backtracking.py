d48bfed9a30b6c0707c03105bc9b8e80302e2230
jesus <jesusromanm99@fpuna.edu.py>
2021-08-08 02:51:45 -0400

    finding all solution

--- backtracking.py
+++ backtracking.py
@@ -1,16 +1,43 @@
+#librerias
 import array
-#import numpy as np
+import copy
+import timeit
+
+#variables globales
 N=4
+list_solution=[]
+visited_nodes=0
+
+#
+
+def draw_solution(list_solution):
+    for i in range(len(list_solution)):
+        print("Esta es la solucion nr:",i+1,"\n")
+        line=""
+        for j in range(len(list_solution[i])):
+            newLine="  |"
+            for k in range(len(list_solution[i])):
+                if(k == list_solution[i][j]):
+                    newLine+="  O"
+                else :
+                    newLine+="  X"
+            newLine+="  |\n"
+            line+=newLine
+        print(line)    
+        print('Vector solucion:',list_solution[i].tolist(),'\n' )
 
 def subs(row1,col1,row2,col2):
+    """ Verifica si dos Array se atacan en una Diagonal"""
     return True if (abs(row1-row2)==abs(col1-col2)) else False
     
 
 def is_valid(row_quen,colum_quen,quen_positions_aux):
+    """Verifica si una nueva reina no es ataca por las demas reinas anteriores"""
+
     #verificamos que no exista dos reinas en la misma fila
     if row_quen in quen_positions_aux:
          return False
-    #[2,0,3,-10]
+
     #verificamos que las reinas no se ataquen en la diagonal
     for quen_position_colum in range(N):
         if(quen_positions_aux[quen_position_colum]!=-10): #verificar que el casillero este ocupado
@@ -20,18 +47,21 @@ def is_valid(row_quen,colum_quen,quen_positions_aux):
             return True
 
 
-def putQuen2(quen_colum,quen_positions,solution):
-    
+def findQuenSolution(quen_colum,quen_positions,solution):
+    """Encuentra una unica solucion para el problema de las N-Reinas"""
+
     if(quen_colum==N):
         return solution
 
-    #print(quen_positions_aux)
-    print('para colum ',quen_colum,'position=',quen_positions,' solution=',solution)
+    global visited_nodes
+    
     for row_quen in range(N):
+        visited_nodes+=1 #aumentamos la cantidad de nodos expandidos
+
         if (is_valid(row_quen,quen_colum,quen_positions)):
             quen_positions[quen_colum]=row_quen
             solution[quen_colum]=row_quen
-            array_solution=putQuen(quen_colum+1,quen_positions,solution)
+            array_solution=findQuenSolution(quen_colum+1,quen_positions,solution)
             if( not -10 in array_solution):
                 return array_solution
     
@@ -40,47 +70,68 @@ def putQuen2(quen_colum,quen_positions,solution):
     return solution
 
 
+def findAllQuenSolution(quen_colum,quen_positions,solution):    
+    """Encuentra todas las soluciones para el problema de las N-Reinas"""
 
-def putQuen(quen_colum,quen_positions,solution):
-    
-    if(quen_colum==N): #ya coloque todas las reinas, tengo una solucion
-        return solution
-
-    colum=quen_colum
-    quen_positions_aux=[quen_positions[i] for i in range(N)]
-    solution_aux=[solution[i] for i in range(N)]
-    #print(quen_positions_aux)
-    print('para colum ',quen_colum,'position=',quen_positions,' solution=',solution)
+    if(quen_colum==N):
+        global list_solution
+        list_solution.append(copy.deepcopy(solution))
+        return
+   
+    global visited_nodes
     for row_quen in range(N):
-        if (is_valid(row_quen,colum,quen_positions_aux)):
-            quen_positions_aux[quen_colum]=row_quen
-            solution_aux[quen_colum]=row_quen
-            array_solution=putQuen(colum+1,quen_positions_aux,solution_aux)
-            if( not -10 in array_solution):
-                return array_solution
+        visited_nodes+=1
+        if (is_valid(row_quen,quen_colum,quen_positions)):
+            quen_positions[quen_colum]=row_quen
+            solution[quen_colum]=row_quen
+            array_solution=findAllQuenSolution(quen_colum+1,quen_positions,solution)
     
-    solution_aux[quen_colum]=-10
-    return solution_aux    
+    quen_positions[quen_colum]=-10
+    solution[quen_colum]=-10
+    
+    return
+
         
 
-def n_quen(n):
+def n_quen():
     global N
-    N=n #definimos el valor de N global
+    N=int(input("Ingrese el tamaño de N:")) 
+    if(N<4):
+        print("N debe ser de un tamaño mayor que 4")
+        return
+    
     quen_positions=array.array('i',[ -10 for i in range(N)]) #inicializamos el array de posiciones con valores -1 
     solution=array.array('i',[ -10 for i in range(N)])
-    print(putQuen2(0,quen_positions,solution)) #colocamos cada reina en la fila correspondiente
+    
+    print(" Seleccione una de las opciones: \n  Opcion 1: Encontrar una unica Solucion \n Opcion 2: Encotrar todas las soluciones     ")
+    option=int(input("Ingrese la Opcion(1 o 2):"))
+   
+    
+    if(option==1):
+        start = timeit.default_timer()
+        findQuenSolution(0,quen_positions,solution)
+        stop = timeit.default_timer()
+        print("\n\nSolucion : ")
+        draw_solution([solution])
+        print('Cantidad de nodos Expandidos:',visited_nodes)
+        print('Tiempo en encontrar solucion: ', stop - start)  
+
+    
+    elif(option==2):
+        start = timeit.default_timer()
+        findAllQuenSolution(0,quen_positions,solution)
+        stop = timeit.default_timer()
+        print('Las soluciones encontradas son :')
+        draw_solution(list_solution)
+        print('Cantidad de soluciones Encontradas:', len(list_solution))
+        print('Cantidad de nodos Expandidos:',visited_nodes)
+        print('Tiempo en encontrar todas las soluciones: ', stop - start)  
+     
+    else:
+        print("Opcion ingresada no valida") 
+    
+
+n_quen()
+#draw_solution([[1,3,0,2]])
 
 
-##Prubas de funciones
-#array_test=array.array('i',[-10,-10,-10,-10])
-#print(is_valid(1,0,array_test))
-n_quen(4)
-"""
-def prueba1(array):
-    array[0]=10
-    print(array)
-    """
-
-#ar=array.array('i',[1,2,3])
-#prueba1(ar)
-#print(ar)
