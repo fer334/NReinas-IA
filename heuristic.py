d80fa0ac643d9c8e2be56a7f5accfae6bbc4aeaa
Fernando Rios <luis.fer334.fa@gmail.com>
2021-08-13 23:27:43 -0400

    improve conflicts calculation

--- heuristic.py
+++ heuristic.py
@@ -2,12 +2,130 @@ import timeit
 import numpy as np
 
 previous_col = np.inf
+debug = False
+printHash = False
 
-def main(N, printMat=True, printHash=True):
 
-    def generate_random_int(max, min):
-        # return Math.floor(Math.random() * (max - min)) + min;
-        return int(np.floor(np.random.rand() * (max - min) + min))
+def generate_random_int(max, min):
+    # return Math.floor(Math.random() * (max - min)) + min;
+    return int(np.floor(np.random.rand() * (max - min) + min))
+
+def print_board(board):
+    N = len(board)
+    for row in range(N):
+        for col in range(N):
+            if printHash:
+                print("Q" if board[row][col]==-1 else "#", end=" ")
+            else:
+                print("Q" if board[row][col]==-1 else board[row][col], end=" ")
+
+        print()
+
+def locate_queens(board):
+    N = len(board)
+    for i in range(N):
+        board[generate_random_int(0,N)][i] = -1
+
+def calc_conflicts(board):
+    N = len(board)
+    # print('board', board)
+    def fill_cols(row, col):
+        # print()
+        for i in range(N):
+            # print(row, i ,board[row][i], end=" * ", sep=" ")
+            if board[row][i] != -1:
+                board[row][i] += 1
+    def fill_rows(row, col):
+        for i in range(N):
+            if board[i][col] != -1:
+                board[i][col] += 1
+    def fill_diags(row, col):
+        i = 1
+        while not (row + i >= N or col + i >= N):
+            if board[row + i][col + i] != -1:
+                board[row + i][col + i] += 1
+            i += 1
+        i = 1
+        while not (row - i < 0 or col - i < 0):
+            if board[row - i][col - i] != -1:
+                board[row - i][col - i] += 1
+            i += 1
+        i = 1
+        while not (row + i >= N or col - i < 0):
+            if board[row + i][col - i] != -1:
+                board[row + i][col - i] += 1
+            i += 1
+        i = 1
+        while not (row - i < 0 or col + i >= N):
+            if board[row - i][col + i] != -1:
+                board[row - i][col + i] += 1
+            i += 1
+    
+    for row in range(N):
+        for col in range(N):
+            if board[row][col] != -1:
+                board[row][col] = 0
+    # print('aueao',board)
+    for col in range(N):
+        for row in range(N):
+            if board[row][col] == -1:
+                fill_cols(row, col)
+                fill_rows(row, col)
+                fill_diags(row, col)
+    # print('hola',board)
+    
+
+def iterate(board):
+    N = len(board)
+    def min_conflict():
+        global previous_col
+        min = np.inf
+        minPos = []
+
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] < min and board[i][j] != -1:
+                    min = board[i][j]
+                    minPos = [{ "i": i, "j": j }]
+                elif board[i][j] == min:
+                    minPos.append({ "i": i, "j": j })
+                    
+        res = minPos[generate_random_int(0, len(minPos))]
+        if (previous_col == res["j"]):
+            board[res["i"]][res["j"]] = 999999
+            return min_conflict()
+    
+        previous_col = res["j"]
+        # print('El min es: ', res)
+        return res
+
+    def queenPosFromCol(pos):
+        row = pos["i"]
+        col = pos["j"]
+        for i in range(N):
+            if board[i][col] == -1:
+                return { "i": i, "j": col }
+
+    def moveQueen(from_pos, to_pos):
+        board[from_pos["i"]][from_pos["j"]] = 0
+        board[to_pos["i"]][to_pos["j"]] = -1
+
+    calc_conflicts(board)
+    # print('chau',board)
+    if debug:
+        print("######Antes de mover")
+        print_board(board)
+    pos = min_conflict()
+    queenPos = queenPosFromCol(pos)
+    # print('queenPos', queenPos)
+    moveQueen(queenPos, pos)
+    if (debug):
+        print("######Despues de mover")
+        print_board(board)
+
+def is_solved(board):
+    N = len(board)
+
     def queensOnRow(row,col):
         queens=0
         for i in range(N):
@@ -34,7 +152,6 @@ def main(N, printMat=True, printHash=True):
                 queens+=1
             i+=1
         
-
         i = 1
         while (not(row + i >= N or col - i < 0)):
             if (board[row + i][col - i] == -1):
@@ -48,86 +165,25 @@ def main(N, printMat=True, printHash=True):
             i+=1
         
         return queens
-    def print_board(board):
-        for row in range(N):
-            for col in range(N):
-                if printHash:
-                    print("Q" if board[row][col]==-1 else "#", end=" ")
-                else:
-                    print("Q" if board[row][col]==-1 else board[row][col], end=" ")
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == -1:
+                attackQueens = queensOnColumn(i, j) + queensOnRow(i, j) + queensOnDiagonal(i, j)
+                if attackQueens != 0:
+                    return False
+    return True
 
-            print()
-
-    def generate_board():
-        board = np.zeros((N, N), dtype=int)
-        for i in range(N):
-            board[generate_random_int(0,N)][i] = -1
-        return board
-
-    def calc_conflicts():
-        for col in range(N):
-            for row in range(N):
-                if board[row][col] != -1:
-                    board[row][col] = 0
-                    board[row][col] += queensOnRow(row, col)
-                    board[row][col] += queensOnColumn(row, col)
-                    board[row][col] += queensOnDiagonal(row, col)
-
-    def iterate():
-        def min_conflict():
-            global previous_col
-            min = np.inf
-            minPos = []
-            for i in range(N):
-                for j in range(N):
-                    if board[i][j] < min and board[i][j] != -1:
-                        min = board[i][j]
-                        minPos = [{ "i": i, "j": j }]
-                    elif board[i][j] == min:
-                        minPos.append({ "i": i, "j": j })
-            res = minPos[generate_random_int(0, len(minPos))]
-            if previous_col == res["j"]:
-                board[res["i"]][res["j"]] = 9999
-                return min_conflict()
-            previous_col = res["j"]
-            return minPos[ generate_random_int(0,len(minPos)) ]
-        def queenPosFromCol(pos):
-            row = pos["i"]
-            col = pos["j"]
-            for i in range(N):
-                if board[i][col] == -1:
-                    return { "i": i, "j": col }
-        def moveQueen(from_pos, to_pos):
-            board[from_pos["i"]][from_pos["j"]] = 0
-            board[to_pos["i"]][to_pos["j"]] = -1
-
-        calc_conflicts()
-        pos = min_conflict()
-        queenPos = queenPosFromCol(pos)
-        moveQueen(queenPos, pos)
-        # if (printMat) console.log("######Despues de mover");
-        if (printMat):
-            print("######Despues de mover")
-            print_board(board)
-
-    def is_solved():
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == -1:
-                    attackQueens = queensOnColumn(i, j) + queensOnRow(i, j) + queensOnDiagonal(i, j)
-                    if attackQueens != 0:
-                       return False
-        return True
-
-    board = generate_board()
+def n_queens(N):
+    board = np.zeros((N, N), dtype=int)
+    locate_queens(board)
     nodes = 0
     start = timeit.default_timer()
-    while(not is_solved()):
-        iterate()
+    while(not is_solved(board)):
+        iterate(board)
         nodes += 1
     stop = timeit.default_timer()
     print("Nodes: ", nodes)
     print("Time: ", stop - start)
 
 
-main(6, printMat=False, printHash=False)
+n_queens(100)
